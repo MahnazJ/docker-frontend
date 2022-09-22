7090bb549df8cdcce102152150683f567fda2ca1
Mahnaz <md.jazayeri.sev@gmail.com>
2022-09-22 13:08:57 +0200

    ...

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         another change on feature
+         another change on featurE
         </p>
         <a
           className="App-link"
