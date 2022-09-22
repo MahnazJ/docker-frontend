b257338f988110cc6eaebca1091755e482e151ae
Mahnaz <md.jazayeri.sev@gmail.com>
2022-09-22 14:57:39 +0200

    added github actions config

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         test
+         test with github actions
         </p>
         <a
           className="App-link"
